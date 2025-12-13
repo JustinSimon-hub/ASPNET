abcb20887104993b7599c438791f1fa6bc4e4064
Justin Simon <justinsimoncs@gmail.com>
2025-12-13 10:45:26 -0800

    Added newer project reference

---
+++ Testing/Models/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Data;
+using System.Collections.Generic;
+using Dapper;
+
+namespace Testing.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+        public ProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+       public IEnumerable<Products> GetAllProducts()
+        {
+            return _connection.Query<Products>("SELECT * FROM Products");
+        }
+    }
+}
---
+++ Testing/Models/Products.cs
@@ -0,0 +1,18 @@
+﻿namespace Testing.Models
+{
+    public class Products
+    {
+        public Products()
+        {
+            
+        }
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public int CategoryID { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+
+
+    }
+}
