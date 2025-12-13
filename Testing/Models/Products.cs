8aee8da3bf6033856f0797b42b4aa96adc81d20c
Justin Simon <justinsimoncs@gmail.com>
2025-12-13 10:57:22 -0800

    Completed step 1

--- Testing/Models/Products.cs
+++ Testing/Models/Products.cs
@@ -9,6 +9,9 @@
 
         public int ProductID { get; set; }
         public string Name { get; set; }
+
+        public double  Price  { get; set; }
+
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
@@ -16,3 +19,5 @@
 
     }
 }
+
+
